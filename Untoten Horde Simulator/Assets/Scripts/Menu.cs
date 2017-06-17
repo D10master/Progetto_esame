139a42288d376c849a2dd7516f183613a7b59bc6
Francesco Romeo <chicco.romeo@alice.it>
2017-06-17 13:33:15 +0200

    aggiunto drop di oggetti

--- Untoten Horde Simulator/Assets/Scripts/Menu.cs
+++ Untoten Horde Simulator/Assets/Scripts/Menu.cs
@@ -3,6 +3,14 @@
 public class Menu : MonoBehaviour
 {
 
+	void Update()
+	{
+		if (Input.GetButtonDown ("Escape"))
+		{
+			Application.Quit ();
+		}
+	}
+
     public void Play()
     {
         Application.LoadLevel(1);
